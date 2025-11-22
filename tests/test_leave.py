7e006a56c4c26d0ba27a43802b299d1eeef44d04
kosmo <kosmos_sega@mail.ru>
2025-11-22 15:30:32 +0300

    Initial commit

--- tests/test_leave.py
+++ tests/test_leave.py
@@ -1,16 +1,51 @@
+from selenium.webdriver.support.wait import WebDriverWait
 from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_assign_leave(driver):
-    LoginPage(driver).open()
-    LoginPage(driver).login("Admin", "admin123")
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.login("Admin", "admin123")
+
+    wait = WebDriverWait(driver, 10)
+
+    leave_menu = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(.,'Leave')]")))
+    leave_menu.click()
+
+    assign_leave_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(.,'Assign Leave')]")))
+    assign_leave_link.click()
+
+    wait.until(EC.presence_of_element_located((By.XPATH, "//h6[text()='Assign Leave']")))
+
+    employee_field = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Type for hints...']")))
+    employee_field.send_keys("John")
     time.sleep(2)
-    driver.find_element(By.XPATH, "//a[contains(.,'Leave')]").click()
-    time.sleep(1)
-    driver.find_element(By.XPATH, "//button[contains(.,'Assign Leave')]").click()
-    driver.find_element(By.XPATH, "//input[@placeholder='Type for hints...']").send_keys("Test User")
-    driver.find_element(By.XPATH, "//input[@placeholder='yyyy-mm-dd']").send_keys("2025-12-01")
-    driver.find_element(By.XPATH, "//button[@type='submit']").click()
-    time.sleep(2)
-    assert "success" in driver.page_source.lower() or "assigned" in driver.page_source.lower()
+
+    suggestion = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='option'][1]")))
+    suggestion.click()
+
+    leave_type_dropdown = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//label[text()='Leave Type']/following::div[1]")))
+    leave_type_dropdown.click()
+
+    vacation_option = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@role='option']//span[text()='CAN - Vacation']")))
+    vacation_option.click()
+
+    date_inputs = driver.find_elements(By.XPATH, "//input[@placeholder='yyyy-dd-mm']")
+    date_inputs[0].clear()
+    date_inputs[0].send_keys("2025-12-01")
+    date_inputs[1].clear()
+    date_inputs[1].send_keys("2025-12-03")   # на этом этапе ошибка, эта дата записается плюсом к дате по умолчанию, т.е. clear не работает
+
+    submit_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+    submit_btn.click()
+
+    success_msg = wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//div[contains(@class, 'oxd-toast')]//p[contains(text(), 'Success')]")))
+
+    assert "Success" in success_msg.text
